bef801106d0071003c0d32e22ef13701c039503e
Maciej Gruszczyński <228131@student.pwr.edu.pl>
2017-07-11 21:26:35 +0200

    dummy html downloading methods

--- src/main/java/maciek/typer/TyperMain.java
+++ src/main/java/maciek/typer/TyperMain.java
@@ -1,5 +1,6 @@
 package maciek.typer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,13 +10,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class TyperMain implements CommandLineRunner{
 
+    @Autowired
+    URLDownloader urlDownloader;
+
     @Override
     public void run(String... args) throws Exception {
+        urlDownloader.dowload();
+        urlDownloader.downloadSource();
+        urlDownloader.dowloadHtml();
         main(args);
     }
 
     public static void main(String[] args) {
         System.out.println("i'm alive");
+
 
     }
 
--- src/main/java/maciek/typer/URLDownloader.java
+++ src/main/java/maciek/typer/URLDownloader.java
@@ -1,11 +1,13 @@
 package maciek.typer;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
@@ -15,7 +17,10 @@ import java.nio.channels.ReadableByteChannel;
 @Component
 public class URLDownloader {
 
-    public static void dowload(){
+    public void dowload(){
+
+
+
 
         try {
             URL website = new URL("https://www.efortuna.pl/pl/strona_glowna/2017-07-11?showActions=1&limit=100&tickLeft=0&selectDates=1&tickRight=0");
@@ -27,5 +32,35 @@ public class URLDownloader {
         catch(IOException e){e.printStackTrace();}
     }
 
+    public void downloadSource(){
+        try {
+
+            URL website = new URL("https://www.efortuna.pl/pl/strona_glowna/2017-07-11?showActions=1&limit=100&tickLeft=0&selectDates=1&tickRight=0");
+            URLConnection conn = website.openConnection();
+            conn.setRequestProperty("agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20300316 Firefox/3.6.2");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    conn.getInputStream(), "UTF-8"));
+            Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("filename.txt"), "utf-8")) ;
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                writer.write(inputLine+'\n');
+            in.close();
+
+        }
+        catch(IOException e){e.printStackTrace();}
+    }
+
+    public void dowloadHtml(){
+        try {
+            Document document = Jsoup.connect("https://www.efortuna.pl/pl/strona_glowna/2017-07-11?showActions=1&limit=100&tickLeft=0&selectDates=1&tickRight=0").get();
+            Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("filename2.txt"), "utf-8")) ;
+            writer.write(document.outerHtml());
+            System.out.println(document.text());
+        }
+        catch(IOException e){e.printStackTrace();}
+
+    }
 
 }
