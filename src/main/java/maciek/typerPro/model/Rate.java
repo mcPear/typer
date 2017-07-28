3ac3c9fb7b912cce84f0a7fd2fa09154965a94dc
Maciej Gruszczyński <228131@student.pwr.edu.pl>
2017-07-28 23:59:44 +0200

    pro Rate::getStatus

--- src/main/java/maciek/typerPro/model/Rate.java
+++ src/main/java/maciek/typerPro/model/Rate.java
@@ -15,4 +15,30 @@ import java.math.BigDecimal;
 public class Rate {
     private BigDecimal value;
     private FootballMatch footballMatch;
+
+    public String getStatus(){
+        String status;
+        BigDecimal smallRate;
+        BigDecimal greatRate;
+        try {
+            smallRate = footballMatch.getRate0().min(footballMatch.getRate1()).min(footballMatch.getRate2());
+            greatRate = footballMatch.getRate0().max(footballMatch.getRate1()).max(footballMatch.getRate2());
+        }
+        catch (NullPointerException e){
+            throw new NullPointerException("Rate::getStatus: One of all three FootballMatch's rates equals null");
+        }
+        try {
+            if (value.equals(smallRate)) {
+                status = "small";
+            } else if (value.equals(greatRate)) {
+                status = "great";
+            } else {
+                status = "draw";
+            }
+        }
+        catch (NullPointerException e){
+            throw new NullPointerException("Rate::getStatus: The value of this Rate equals null");
+        }
+        return status;
+    }
 }
